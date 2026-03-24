4878023d5722072a3e0165a62c6eca3225f60fd9
Gustav von Knorring <gusvo087@student.liu.se>
2026-03-24 14:19:56 +0100

    removed empty files

--- client_simplified/app_install.js
+++
@@ -1,3 +0,0 @@
-/* logic for crypto sign the loaded .wgt file, and then install it on the system.
- - target: Tizen +5.5 and later : Chromium 69 / M69
- */
--- client_simplified/script.js
+++
@@ -1,3 +0,0 @@
-/* logic for the interface and github api to get .wgt files
- - target: Tizen +5.5 and later : Chromium 69 / M69
- */
